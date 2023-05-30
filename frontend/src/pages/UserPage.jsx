2f9410246f1fd656c8aaa12b7b8ff3bc4d701f81
chrillah <ch.ronnberg@gmail.com>
2023-05-30 11:23:42 +0200

    user-page updated

--- frontend/src/pages/UserPage.jsx
+++ frontend/src/pages/UserPage.jsx
@@ -25,7 +25,8 @@ function UserPage() {
     <><TopContainer />
     <PresentationContainer>
         <Presentation>
-            <h1>{user.firstname}</h1>
+            <h1 className='user-title'>{user.firstname} {user.lastname}</h1>
+            <h1 className='user-email'>{user.email}</h1>
         </Presentation>
     </PresentationContainer>
     </>
@@ -35,16 +36,27 @@ function UserPage() {
 export default UserPage
 
 const TopContainer = styled.div`
-min-height: 300px;
+min-height: 200px;
 background-color: ${Colors.BLUE};
 `
 const PresentationContainer = styled.div`
+font-family: 'Poppins', sans-serif;
 display: flex;
 justify-content: center;
 align-items: center;
-
+background-color: ${Colors.GREY};
+min-height: 200px;
+border-radius: 0 0 50px 50px;
 `
 const Presentation = styled.div`
 width: 100%;
-max-width: 500px
+max-width: 500px;
+.user-title{
+    font-weight: bold;
+}
+
+.user-email{
+    font-weight: 500;
+    color: ${TextColor.LIGHTER}
+}
 `
