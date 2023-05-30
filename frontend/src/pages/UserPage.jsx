7ff11da8e6d1de2669a6814fdb169c7f47f9a7bd
chrillah <ch.ronnberg@gmail.com>
2023-05-30 09:57:16 +0200

    userpage added

---
+++ frontend/src/pages/UserPage.jsx
@@ -0,0 +1,50 @@
+import React,{useEffect, useState} from 'react'
+import axios from 'axios';
+import {useParams} from 'react-router-dom'
+import styled from 'styled-components'
+import { Colors, TextColor } from '../styles';
+
+function UserPage() {
+
+    const {paramsId} = useParams()
+
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://localhost:8800/users/${paramsId}`)
+          .then(response => {
+            setUser(response.data);
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }, []);
+
+
+  return (
+    <><TopContainer />
+    <PresentationContainer>
+        <Presentation>
+            <h1>{user.firstname}</h1>
+        </Presentation>
+    </PresentationContainer>
+    </>
+  )
+}
+
+export default UserPage
+
+const TopContainer = styled.div`
+min-height: 300px;
+background-color: ${Colors.BLUE};
+`
+const PresentationContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+
+`
+const Presentation = styled.div`
+width: 100%;
+max-width: 500px
+`
